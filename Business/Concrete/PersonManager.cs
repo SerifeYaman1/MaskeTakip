be894f389f32a532c5b1b958d84d0679159cabc2
SerifeYaman1 <serifeaacyaman12@gmail.com>
2024-02-11 17:05:19 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager
+    {
+        // Encapsulation: Propertyleri alıp bir classın içine koymaya denir.
+        // Maske için başvuru alınan metot 
+        public void ApplyForMask(Person person) // Person nesnesini eklemek için add references to entities
+        { 
+
+        
+        }
+        // Şu ana kadar kimler başvuruda bulunmuş o kişilerin listesi
+        public List<Person> GetList() 
+        {
+            return null;
+        }
+    }
+}
